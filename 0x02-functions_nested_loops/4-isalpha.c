4ea225deef2b4dc0e3bf8c906a2b0045ebc55aa3
Rasheed Izuagbe <imoesii70@gmail.com>
2022-10-20 20:03:02 -0700

    correctionss

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,16 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	char lower, upper;
+	int isletter = 0;
 
-			return (1);
-		else
-			return (0);
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				isletter = 1;
+		}
+	}
+	return (isletter);
 }
